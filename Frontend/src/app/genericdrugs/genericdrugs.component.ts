dca2b1bb575c05e1f0ad224626c0e9d1daef17d5
Victor Brum <83709796+victor-brum@users.noreply.github.com>
2025-03-09 12:39:47 -0300

    Add item selection

--- Frontend/src/app/genericdrugs/genericdrugs.component.ts
+++ Frontend/src/app/genericdrugs/genericdrugs.component.ts
@@ -12,13 +12,22 @@ import { TitleComponent } from "../title/title.component";
 export class GenericdrugsComponent {
 
   public title = 'Generic Drugs'
+  public selectedGenericdrug?: string;
 
   public genericdrugsList = [
-    { id: 1, name: 'Novalgina', Active_ingredient: 'null', Laboratory: 'null', Expiration_Date: '12/12/2025', Quantity: 1, Price: '12,90'},
-    { id: 2, name:'Rivotril', Active_ingredient: 'null', Laboratory: 'null', Expiration_Date: '12/12/2025', Quantity: 1, Price: '12,90' },
-    { id: 3, name:'Dorflex', Active_ingredient: 'null', Laboratory: 'null', Expiration_Date: '12/12/2025', Quantity: 1, Price: '12,90' },
-    { id: 4, name:'Ritalina', Active_ingredient: 'null', Laboratory: 'null', Expiration_Date: '12/12/2025', Quantity: 1, Price: '12,90' },
-    { id: 5, name:'Sivastatina', Active_ingredient: 'null', Laboratory: 'null', Expiration_Date: '12/12/2025', Quantity: 1, Price: '12,90' },
-    { id: 6, name:'Zolpidem', Active_ingredient: 'null', Laboratory: 'null', Expiration_Date: '12/12/2025', Quantity: 1, Price: '12,90' },
+    { id: 1, name: 'Novalgina', Active_ingredient: 'null', Laboratory: 'null', Expiration_Date: '12/12/2025', Quantity: 1, Price: '$12,90'},
+    { id: 2, name:'Rivotril', Active_ingredient: 'null', Laboratory: 'null', Expiration_Date: '12/12/2025', Quantity: 1, Price: '$12,90' },
+    { id: 3, name:'Dorflex', Active_ingredient: 'null', Laboratory: 'null', Expiration_Date: '12/12/2025', Quantity: 1, Price: '$12,90' },
+    { id: 4, name:'Ritalina', Active_ingredient: 'null', Laboratory: 'null', Expiration_Date: '12/12/2025', Quantity: 1, Price: '$12,90' },
+    { id: 5, name:'Sivastatina', Active_ingredient: 'null', Laboratory: 'null', Expiration_Date: '12/12/2025', Quantity: 1, Price: '$12,90' },
+    { id: 6, name:'Zolpidem', Active_ingredient: 'null', Laboratory: 'null', Expiration_Date: '12/12/2025', Quantity: 1, Price: '$12,90' },
   ];
+
+  back() {
+    this.selectedGenericdrug = '';
+  }
+
+  selectGenericdrug(genericdrug : any) {
+    this.selectedGenericdrug = genericdrug.name;
+  }
 }
