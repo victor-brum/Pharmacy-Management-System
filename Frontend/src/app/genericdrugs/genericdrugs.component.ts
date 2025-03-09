fdae2af349133bb4d5dbfa38f66163b59d2c16db
Victor Brum <83709796+victor-brum@users.noreply.github.com>
2025-03-09 19:09:14 -0300

    Remove name from generic drug list

--- Frontend/src/app/genericdrugs/genericdrugs.component.ts
+++ Frontend/src/app/genericdrugs/genericdrugs.component.ts
@@ -15,12 +15,12 @@ export class GenericdrugsComponent {
   public selectedGenericdrug?: string;
 
   public genericdrugsList = [
-    { id: 1, name: 'Novalgina', Active_ingredient: 'null', Laboratory: 'null', Expiration_Date: '12/12/2025', Quantity: 1, Price: '$12,90'},
-    { id: 2, name:'Rivotril', Active_ingredient: 'null', Laboratory: 'null', Expiration_Date: '12/12/2025', Quantity: 1, Price: '$12,90' },
-    { id: 3, name:'Dorflex', Active_ingredient: 'null', Laboratory: 'null', Expiration_Date: '12/12/2025', Quantity: 1, Price: '$12,90' },
-    { id: 4, name:'Ritalina', Active_ingredient: 'null', Laboratory: 'null', Expiration_Date: '12/12/2025', Quantity: 1, Price: '$12,90' },
-    { id: 5, name:'Sivastatina', Active_ingredient: 'null', Laboratory: 'null', Expiration_Date: '12/12/2025', Quantity: 1, Price: '$12,90' },
-    { id: 6, name:'Zolpidem', Active_ingredient: 'null', Laboratory: 'null', Expiration_Date: '12/12/2025', Quantity: 1, Price: '$12,90' },
+    { id: 1, Active_ingredient: 'null', Laboratory: 'null', Expiration_Date: '12/12/2025', Quantity: 1, Price: '$12,90'},
+    { id: 2, Active_ingredient: 'null', Laboratory: 'null', Expiration_Date: '12/12/2025', Quantity: 1, Price: '$12,90' },
+    { id: 3, Active_ingredient: 'null', Laboratory: 'null', Expiration_Date: '12/12/2025', Quantity: 1, Price: '$12,90' },
+    { id: 4, Active_ingredient: 'null', Laboratory: 'null', Expiration_Date: '12/12/2025', Quantity: 1, Price: '$12,90' },
+    { id: 5, Active_ingredient: 'null', Laboratory: 'null', Expiration_Date: '12/12/2025', Quantity: 1, Price: '$12,90' },
+    { id: 6, Active_ingredient: 'null', Laboratory: 'null', Expiration_Date: '12/12/2025', Quantity: 1, Price: '$12,90' },
   ];
 
   back() {
@@ -28,11 +28,11 @@ export class GenericdrugsComponent {
   }
 
   selectGenericdrug(genericdrug : any) {
-    this.selectedGenericdrug = genericdrug.name;
+    this.selectedGenericdrug = genericdrug.Active_ingredient;
   }
 
   selectBranddrug(genericdrug: any) {
-    this.selectedGenericdrug = genericdrug.name;
+    this.selectedGenericdrug = genericdrug.Active_ingredient;
   }
 
    formatPrice(event: any) {
